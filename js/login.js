ca488e100164a4d28971e2528b9cb5a615a1a1fe
shing <apple@SKYPPK.local>
2016-12-03 06:37:02 +0800

    login

---
+++ js/login.js
@@ -0,0 +1,5 @@
+$('.zmdi-account-add').click(function(e) {
+  $('.fab').toggleClass('active');
+  $('.fab').children('i').toggleClass('zmdi-account-add');
+  $('.fab').children('i').toggleClass('zmdi-close');
+});
